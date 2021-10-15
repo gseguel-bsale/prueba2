9aa61070ae5ba2666c63c66d279780c200b2adbe
gseguel-bsale <gseguel@imaginex.cl>
2021-10-14 21:51:55 -0300

    Primer Commit

---
+++ appecommerce.rb
@@ -0,0 +1,69 @@
+require "sinatra"
+require "mysql2"
+require "json"
+require "sinatra"
+require "sinatra/cross_origin"
+
+#control de acceso bloqeo pagina web.
+configure do
+  enable :cross_origin
+end
+
+#control de acceso bloqeo pagina web.
+options '/*' do
+    content_type :json
+    headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, OPTIONS"
+    headers['Access-Control-Allow-Headers'] ="accept, authorization, origin"
+end
+
+
+db = Mysql2::Client.new(:host => "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com", :username => "bsale_test", :password => "bsale_test", :database => "bsale_test")
+
+before do
+    content_type :json
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin'
+end
+
+
+#aca hay que mostrar las rutas
+#la prueba dice listar por categoria
+
+
+#filtro para productos productos.
+get '/products' do # si viene parametro concatenar con un where para hacer filtro de producc to
+    product_filter = ""
+    if params["name"]  != nil
+        product_filter = "name like '%#{params["name"]}%'"
+    end   
+    product_query = "select * from product where  #{product_filter}"
+    puts product_query
+    query_products = db.query(product_query) 
+    product_to_json(query_products)
+end
+
+#select filtro categoria y productoss pagina inicial
+get '/' do 
+    query_categories = db.query 'SELECT category.id, category.name as categoria, product.name, product.url_image, product.price, product.discount from product INNER JOIN category ON product.category = category.id;'
+    product_to_json(query_categories)
+end
+
+#filtro para categorias.
+get '/categories' do 
+    query_categories = db.query 'SELECT * from  category;'
+    product_to_json(query_categories)
+end
+
+def product_to_json(array)
+    json_array = []
+    array.each do |element|
+        json_array.push(element)
+        
+    end
+    return JSON.generate({ :data => json_array})
+end
+
+
+
---
+++ index.js
@@ -0,0 +1,69 @@
+const API_URL = "http://127.0.0.1:4567";
+
+const xhttp = new XMLHttpRequest();
+xhttp.addEventListener("load", obtenerProductosClasificadosIniciales);
+xhttp.open("GET", API_URL);
+xhttp.send();
+
+function obtenerProductosClasificadosIniciales() {
+  if (this.readyState == 4 && this.status == 200) {
+    let dataJson = JSON.parse(this.responseText);
+    //console.log(dataJson["data"][0]);
+
+    //obtener productos iniciales.
+    for (let i = 0; i < dataJson["data"].length; i++) {
+      let unDataJson = dataJson["data"][i];
+      
+      
+      console.log(unDataJson);
+      
+      //solo agregare productos con foto 
+      if (dataJson["data"][i]["url_image"] != null && dataJson["data"][i]["url_image"] != "") {
+        let imagen = document.getElementById("imagen-productos");
+
+        imagen.innerHTML += "<tr><td><img src='" +unDataJson["url_image"] +"' width='250' height='250'></td><td><p>Precio: $"+unDataJson["price"] +"</p></tr></td>";
+      }
+    }
+    //obteniendo info del boton
+    const botones = document.querySelectorAll(".btn");
+    let botonSelecionado = "";
+
+    //funcion cuando se hace clic en el boton
+    const cuandoSeHaceClick = function (evento) {
+      //obtengo el boton.
+      botonSelecionado = this.innerText;
+      //console.log(botonSelecionado);
+      let arrayCat = ["bebida energetica","pisco","ron","bebida","snack","cerveza","vodka"];
+      for (let i = 0; i < arrayCat.length; i++) {
+        if (botonSelecionado == arrayCat[i]) {
+          botonSelecionado = i + 1;
+          console.log(
+            "el id es: " + botonSelecionado + " la categoria es: " + arrayCat[i]
+          );
+        }
+
+        //agregando imagenes por  filtro de categoria.
+        let imagen2 = document.getElementById("imagen-productos");
+        imagen2.innerHTML = "";
+        for (let i = 0; i < dataJson["data"].length; i++) {
+          let unDataJson = dataJson["data"][i];
+
+          //let app = document.getElementById('app');
+          //app.innerHTML += '<br>Categoria: ' + unDataJson['categoria'] + '<br> Producto: ' + unDataJson['name'];
+          console.log(botonSelecionado + " hola");
+          if (
+            dataJson["data"][i]["id"] == botonSelecionado &&
+            dataJson["data"][i]["url_image"] != null &&
+            dataJson["data"][i]["url_image"] != ""
+          ) {
+            imagen2.innerHTML += "<tr><td><img src='" +unDataJson["url_image"] +"' width='250' height='250'></td><td><p>Precio: $"+unDataJson["price"] +"</p></tr></td>"
+          }
+        }
+      }
+    };
+    botones.forEach((boton) => {
+      //Agregar listener
+      boton.addEventListener("click", cuandoSeHaceClick);
+    });
+  }
+}
