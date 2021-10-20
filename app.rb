92caa4b47ce0db160fb640d6d3e11a7ee6236cbd
gseguel-bsale <gseguel@imaginex.cl>
2021-10-20 14:05:12 -0300

    first commit

--- app.rb
+++ app.rb
@@ -1,33 +1,31 @@
 require "sinatra"
 require "mysql2"
 require "json"
-require "sinatra"
-require "sinatra/cross_origin"
-require "rack"
+
 
 
 #control de acceso bloqeo pagina web.
-configure do
-  enable :cross_origin
-end
+#configure do
+#  enable :cross_origin
+#end
 
 #control de acceso bloqeo pagina web.
-options '/*' do
-    content_type :json
-    headers['Access-Control-Allow-Origin'] = "*"
-    headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, OPTIONS"
-    headers['Access-Control-Allow-Headers'] ="accept, authorization, origin"
-end
+#options '/*' do
+#    content_type :json
+#    headers['Access-Control-Allow-Origin'] = "*"
+#    headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, OPTIONS"
+#    headers['Access-Control-Allow-Headers'] ="accept, authorization, origin"
+#end
 
 
 db = Mysql2::Client.new(:host => "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com", :username => "bsale_test", :password => "bsale_test", :database => "bsale_test")
 
-before do
-    content_type :json
-    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin'
-end
+#before do
+#    content_type :json
+##    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+#    headers['Access-Control-Allow-Origin'] = '*'
+#    headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin'
+#end
 
 
 #aca hay que mostrar las rutas
@@ -35,16 +33,16 @@ end
 
 
 #filtro para productos productos.
-get '/products' do # si viene parametro concatenar con un where para hacer filtro de producc to
-    product_filter = ""
-    if params["name"]  != nil
-        product_filter = "name like '%#{params["name"]}%'"
-    end   
-    product_query = "select * from product where  #{product_filter}"
-    puts product_query
-    query_products = db.query(product_query) 
-    product_to_json(query_products)
-end
+#get '/products' do # si viene parametro concatenar con un where para hacer filtro de producc to
+#    product_filter = ""
+##    if params["name"]  != nil
+#       product_filter = "name like '%#{params["name"]}%'"
+#    end   
+#    product_query = "select * from product where  #{product_filter}"
+#    puts product_query
+#    query_products = db.query(product_query) 
+#    product_to_json(query_products)
+#end
 
 #select filtro categoria y productoss pagina inicial
 get '/' do 
